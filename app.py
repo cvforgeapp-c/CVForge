ace61f7c6a27cde29ee5b3cc721dd44d099b567f
cvforgeapp-c <allhuwakiil@gmail.com>
2026-09-15 18:00:54 +0300

    Update app.py

--- app.py
+++ app.py
@@ -3542,8 +3542,8 @@ def generate():
 )
     }
 
-    photo = 
-request.files.get("photo")
+        photo = 
+    request.files.get("photo")
 
     if photo and photo.filename:
         photo_path = os.path.join(
