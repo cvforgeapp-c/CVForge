7441e6fdeef2e1e155502258345b0782c921295d
cvforgeapp-c <allhuwakiil@gmail.com>
2026-09-15 23:57:00 +0300

    Update app.py

--- app.py
+++ app.py
@@ -2929,7 +2929,7 @@ def generate_modern(c, data):
             # -------------------------------------------------
 
             c.setStrokeColor(accent)
-            c.setLineWidth(3.5)
+            c.setLineWidth(2.5) 
 
             c.circle(
                 photo_cx,
