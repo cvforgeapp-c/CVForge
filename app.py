439825119aad166285dc271b7d02cf02c1113346
cvforgeapp-c <allhuwakiil@gmail.com>
2026-09-15 15:56:22 +0300

    Update app.py

--- app.py
+++ app.py
@@ -2875,7 +2875,7 @@ def generate_modern(c, data):
 
         
         photo_cx = 39 * mm
-        photo_cy = header_bottom + 16 * mm
+        photo_cy = header_bottom + 15 * mm
 
         radius = photo_size / 2
 
