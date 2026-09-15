881cf66be93d632efdd7eec72ea9389f365dbd28
cvforgeapp-c <allhuwakiil@gmail.com>
2026-09-15 21:32:51 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1832,12 +1832,10 @@ band_h = 13 * mm
 
 header_bottom = H - header_h
 band_y = header_bottom - band_h
-
     # =========================================================
     # WHITE HEADER
     # =========================================================
-
-    c.setFillColor(white)
+c.setFillColor(white)
 
     c.rect(
         0,
