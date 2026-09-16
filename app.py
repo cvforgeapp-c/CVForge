e1f42ae6413cb77fcff87a804411090cc3b63729
cvforgeapp-c <allhuwakiil@gmail.com>
2026-09-16 22:48:59 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1878,7 +1878,7 @@ def generate_modern(c, data):
     name = clean(data.get("name")) or "Your Name"
 
     # Position just to the right of the photo
-    name_x = 82 * mm
+    name_x = photo_right + 6 * mm
     name_y = H - 25 * mm
 
        # Large bold name
