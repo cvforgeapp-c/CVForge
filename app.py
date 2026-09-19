cf0147c040018c046a2b6b726762e14372ee16fd
cvforgeapp-c <allhuwakiil@gmail.com>
2026-09-19 12:40:43 +0300

    Update app.py

--- app.py
+++ app.py
@@ -2157,7 +2157,7 @@ def modern(data,file):
         c.setLineWidth(1.1)
 
         c.line(
-            x,
+            x + 12 * mm,
             y - 2.5 * mm,
             x + width,
             y - 2.5 * mm
