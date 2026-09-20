eb4d3be89c87a4d33747580d98cf069c4e01a602
cvforgeapp-c <allhuwakiil@gmail.com>
2026-09-20 16:43:31 +0300

    Update app.py

--- app.py
+++ app.py
@@ -3456,10 +3456,10 @@ def generate():
         "languages": request.form.get("languages", ""),
         "hobbies": request.form.get("hobbies", ""),
         "references": request.form.get("references", ""),
+        "signature": signature,
         "template": request.form.get("template", "modern"),
 "accent_color": request.form.get("accent_color"),
 "sidebar_color": request.form.get("sidebar_color", "#173F49"),
-        "include_signature": request.form.get("include_signature") == "on",
 
     }
     
