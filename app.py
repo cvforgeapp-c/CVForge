939a677ccca1a2952834a5ec4521fee53a52e155
cvforgeapp-c <allhuwakiil@gmail.com>
2026-09-21 21:23:24 +0300

    Update app.py

--- app.py
+++ app.py
@@ -2787,7 +2787,7 @@ def modern(data,file):
                     bullet=True
                 )
 
-                y -= 2 * mm
+                y -= 1 * mm
 
         # Space after the complete Experience section
         y -= 5 * mm
