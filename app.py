66fb8e6e53dbf1a4f17ebec68504fb4d503094be
cvforgeapp-c <allhuwakiil@gmail.com>
2026-09-23 16:50:53 +0300

    Update app.py

--- app.py
+++ app.py
@@ -2511,71 +2511,71 @@ def modern(data,file):
 
         # location
         elif kind == "location":
-        c.setLineWidth(1.2)
+            c.setLineWidth(1.2)
 
-        c.circle(
-            cx,
-            cy + 1.2*mm,
-            2.0*mm,
-            fill=0,
-            stroke=1
-        )
+            c.circle(
+                cx,
+                cy + 1.2*mm,
+                2.0*mm,
+                fill=0,
+                stroke=1
+            )
 
-        p = c.beginPath()
-        p.moveTo(cx - 3.2*mm, cy + 1.0*mm)
-        p.curveTo(
-            cx - 3.2*mm, cy - 1.8*mm,
-            cx,
-            cy - 4.0*mm,
-            cx,
-            cy - 4.0*mm
-        )
-        p.curveTo(
-            cx,
-            cy - 4.0*mm,
-            cx + 3.2*mm,
-            cy - 1.8*mm,
-            cx + 3.2*mm,
-            cy + 1.0*mm
-        )
-        c.drawPath(p, fill=0, stroke=1)
+            p = c.beginPath()
+            p.moveTo(cx - 3.2*mm, cy + 1.0*mm)
+            p.curveTo(
+                cx - 3.2*mm, cy - 1.8*mm,
+                cx,
+                cy - 4.0*mm,
+                cx,
+                cy - 4.0*mm
+            )
+            p.curveTo(
+                cx,
+                cy - 4.0*mm,
+                cx + 3.2*mm,
+                cy - 1.8*mm,
+                cx + 3.2*mm,
+                cy + 1.0*mm
+            )
+            c.drawPath(p, fill=0, stroke=1)
 
 
         # LinkedIn
         elif kind == "linkedin":
-        c.setFont("Helvetica-Bold", 6.5)
-        c.drawCentredString(
-            cx,
-            cy - 2.2*mm,
-            "in"
-        )
+            c.setFont("Helvetica-Bold", 6.5)
+            c.drawCentredString(
+                cx,
+                cy - 2.2*mm,
+                "in"
+            )
 
 
         # website
         elif kind == "website":
-        c.circle(
-            cx,
-            cy,
-            3.5*mm,
-            fill=0,
-            stroke=1
-        )
+            c.circle(
+                cx,
+                cy,
+                3.5*mm,
+                fill=0,
+                stroke=1
+            )
 
-        c.ellipse(
-            cx - 1.7*mm,
-            cy - 3.5*mm,
-            cx + 1.7*mm,
-            cy + 3.5*mm,
-            fill=0,
-            stroke=1
-        )
+            c.ellipse(
+                cx - 1.7*mm,
+                cy - 3.5*mm,
+                cx + 1.7*mm,
+                cy + 3.5*mm,
+                fill=0,
+                stroke=1
+            )
 
-        c.line(
-            cx - 3.5*mm,
-            cy,
-            cx + 3.5*mm,
-            cy
-        )
+            c.line(
+                cx - 3.5*mm,
+                cy,
+                cx + 3.5*mm,
+                cy
+            )
 
         # SKILLS / GEAR
         elif icon_kind == "skills":
